3f971e4e9978ab5e98832189baab923aa6bb8e01
kariiixdev <kariiixdev@proton.me>
2024-12-22 14:17:48 +0100

    ✅ Update bootloader

--- src/boot.s
+++ src/boot.s
@@ -1,16 +1,24 @@
 .intel_syntax noprefix
 .code16
 
-.section .text
 .global _start
 
 _start:
-    mov ah, 0x0E
-    mov al, 'A'
-    mov bx, 0
+    mov si, offset msg
+    mov ah, 0x00
+    mov al, 0x0D
     int 0x10
 
-    jmp .
-
+    mov ah, 0x0E
+    mov bl, 0x0C
+print_char:
+    lodsb
+    cmp al, 0
+    je done
+    int 0x10
+    jmp print_char
+done:
+    hlt
+msg: .string "Hello world!"
 .fill 510-(.-_start), 1, 0
 .word 0xAA55
